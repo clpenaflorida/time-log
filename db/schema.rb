ae5bac0e4f98351c01d05649cef03f84da6aa08d
Leah Ylaya <leah@nerubia.com>
2015-06-10 14:20:59 +0800

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608091355) do
+ActiveRecord::Schema.define(version: 20150610054902) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name",   limit: 255
